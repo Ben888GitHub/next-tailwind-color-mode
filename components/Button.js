e85b2ec4dc60bf51b9673a97c416d916d751b165
Benedict <benedictryan80@gmail.com>
2022-09-21 16:15:01 +0700

    change theme to resolvedTheme

--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,16 @@
 import { useTheme } from 'next-themes';
 
 function Button() {
-	const { theme, setTheme } = useTheme();
+	const { theme, setTheme, resolvedTheme } = useTheme();
+
+	console.log(resolvedTheme);
+	console.log(theme);
 	return (
 		<button
 			className="text-3xl text-white bg-gray-600 rounded-md p-5"
-			onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+			onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
 		>
-			{theme === 'dark' ? 'light' : 'dark'}
+			{resolvedTheme === 'dark' ? 'light' : 'dark'}
 		</button>
 	);
 }
